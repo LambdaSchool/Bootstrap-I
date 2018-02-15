b5716c1bdd84871806c2fbf744f1460d577371ca
Brett Sullivan <opticalfalicy@gmail.com>
2018-02-14 18:29:34 -0600

    80% Complete
    Adding javascript capabilities between a button and Table, then will be complete.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("myDiv");
+    if (x.style.display === "none") {
+        x.style.display = "table";
+    } else{
+        x.style.display = "none";
+    }
+}
