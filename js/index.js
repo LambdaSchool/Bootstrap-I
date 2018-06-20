b53a2a7096b270ecad596b3f90fcc8433f2614d9
Ashwin <aswinjeganath@gmail.com>
2018-06-21 04:36:17 +0530

    Add a theatrical entry

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  $('#introduction button').click(function() {
+    $('#introduction').fadeOut('slow', function() {
+      $('#landing .title').fadeTo('slow', 1);
+    });
+  });
+
   $('a[href*="#"').click(function(event) {
     var target = $(this.hash);
     event.preventDefault();
